c02d406cce9ab5b1dac94991cdd4ae5263ace7bf
kareemah <ajimobikareemah@gmail.com>
2023-01-02 10:48:52 +0100

    darkmode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import data from "./data";
 
 function App() {
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") || "dark-theme"
+  );
+  const toggleTheme = () => {
+    if (theme === "dark-theme") {
+      setTheme("light-theme");
+    } else {
+      setTheme("dark-theme");
+    }
+  };
+  useEffect(() => {
+    // if (!theme) {
+    //   setTheme("dark-theme");
+    // }
+    document.body.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn" onClick={toggleTheme}>
+            toggle
+          </button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((item, index) => {
+          const { title, snippet, date, length } = item;
+          return (
+            <article className="post" key={index}>
+              <h2>{title}</h2>
+              <div className="post-info">
+                {/* <span>{moment(date).format("dddd Do, YYYY")}</span> */}
+                <span>{length} min read</span>
+              </div>
+              <p>{snippet}</p>
+            </article>
+          );
+        })}
+      </section>
+    </main>
   );
 }
 
